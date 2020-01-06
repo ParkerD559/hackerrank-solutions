28a5162629df1ac2547db9324c310de3adcdbc94
David Parker <crazdave@gmail.com>
2020-01-06 09:40:40 -0800

    matrix layer rotation

--- problem-solving/hard/matrix-layer-rotation.py
+++ problem-solving/hard/matrix-layer-rotation.py
@@ -5,22 +5,62 @@ Notes:
     [1, 2, 3, 4] -> [2, 3, 4, 1]
 ]
 
-<----------------------- up ------------------------->  <---------------- down -------------->
+<-------------------->  <-------------------->  <-------------------->  <-------------------->
 (0, 0), (1, 0), (2, 0), (3, 0), (3, 1), (3, 2), (3, 3), (2, 3), (1, 3), (0, 3), (0, 2), (0, 1)
-<-------------------->  <---->
+<---->  <---->  <---->  <---->
 (1, 1), (2, 1), (2, 2), (1, 2)
 
 
 """
 
 
-def matrixShells(matrix, m, n):
-    """Returns shells of matrix in clockwise rotation"""
-    num_shells = min(m, n) / 2
-    return [
-        [num[y, x] for x in range(i, n - i) for y in range()] for i in range(num_shells)
+def iter_shell(m, n, s):
+    """Returns coordinates in clockwise order of shell s in m x n matrix"""
+    left_column = top_row = s
+    right_column = n - s - 1
+    bottom_row = m - s - 1
+
+    for column in range(left_column, right_column):
+        yield (column, top_row)  # Top
+    for row in range(top_row, bottom_row):
+        yield (right_column, row)  # Right
+    for column in range(right_column, left_column, -1):
+        yield (column, bottom_row)  # Bottom
+    for row in range(bottom_row, top_row, -1):
+        yield (left_column, row)  # Left
+
+
+def rotate(shell, r):
+    rotations = r % len(shell)
+    return shell[rotations:] + shell[:rotations]
+
+
+def matrix_rotation(matrix, m, n, r):
+    num_shells = min(m, n) // 2
+    shells = [
+        [matrix[y][x] for x, y in iter_shell(m, n, num)] for num in range(num_shells)
     ]
+    rotated = [rotate(shell, r) for shell in shells]
+    for s, shell in enumerate(rotated):
+        for (x, y), val in zip(iter_shell(m, n, s), shell):
+            matrix[y][x] = val
 
 
-def matrixRotation(matrix, m, n, r):
-    pass
+if __name__ == "__main__":
+    mnr = input().rstrip().split()
+
+    m = int(mnr[0])
+
+    n = int(mnr[1])
+
+    r = int(mnr[2])
+
+    matrix = []
+
+    for _ in range(m):
+        matrix.append(list(map(int, input().rstrip().split())))
+
+    matrix_rotation(matrix, m, n, r)
+
+    for row in matrix:
+        print(" ".join(map(str, row)))
